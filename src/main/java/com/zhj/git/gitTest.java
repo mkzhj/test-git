828779500858f8dbdf3570150a55a4ad428423c7
zhj <1540381961@qq.com>
2022-12-10 22:33:38 +0800

    master commit2

--- src/main/java/com/zhj/git/gitTest.java
+++ src/main/java/com/zhj/git/gitTest.java
@@ -6,5 +6,6 @@ public class gitTest {
         System.out.println("hello git222");
         System.out.println("hello git3333");
         System.out.println("正常合并");
+        System.out.println("master分支的测试冲突合并");
     }
 }
