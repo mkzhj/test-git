3add5103e80c8912b6e70a1cb4e9602f1a1ce2dd
zhj <1540381961@qq.com>
2022-12-10 21:56:14 +0800

    second commit

--- src/main/java/com/zhj/git/gitTest.java
+++ src/main/java/com/zhj/git/gitTest.java
@@ -3,5 +3,6 @@ package com.zhj.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git222");
     }
 }
